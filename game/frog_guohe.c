7b9688684f1cbb32b492d3b98070975d306c48df
tom-cf <uipps_com@163.com>
2021-01-21 02:21:02 +0800

    数据结构和算法题目

---
+++ game/frog_guohe.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+
+void print_road(int arrRoad[], int nLen)
+{
+    int i = 0;
+
+    for (i = 0; i < nLen; i++)
+    {
+        if (arrRoad[i] < 0)
+            printf("-> ");
+        else if (arrRoad[i] > 0)
+            printf("<- ");
+        else
+            printf("O ");
+    }
+    printf("\n\n");
+}
+
+void swap(int *nN1, int *nN2)
+{
+    int nTmp = 0;
+
+    nTmp = *nN1;
+    *nN1 = *nN2;
+    *nN2 = nTmp;
+}
+
+// left side the number is less than 0
+// right side the number is bigger than 0
+// empty grid is 0
+int move_frog(int arrRoad[], int nLen, int nEmptyGrid)
+{
+    int i = 0;
+    int bMove = 1;
+    int nSuccessCount = 0;
+
+    // move the left side
+    while (nSuccessCount < nLen - 1)
+    {
+        bMove = 1;
+        for (i = 0; i < nEmptyGrid && bMove; i++)
+        {
+            if ((arrRoad[nEmptyGrid - 2] < 0) && (arrRoad[nEmptyGrid - 1] > 0))
+            {
+                swap(&arrRoad[nEmptyGrid - 2], &arrRoad[nEmptyGrid]);
+                if (nEmptyGrid == nLen - 1)
+                    nSuccessCount++;
+                nEmptyGrid -= 2;
+                print_road(arrRoad, nLen);
+                bMove = 0;
+                break;
+            }
+        }
+
+        for (i = nLen - 1; i > nEmptyGrid && bMove; i--)
+        {
+            if ((arrRoad[nEmptyGrid + 2] > 0) && (arrRoad[nEmptyGrid + 1] < 0))
+            {
+                swap(&arrRoad[nEmptyGrid + 2], &arrRoad[nEmptyGrid]);
+                if (nEmptyGrid == 0)
+                    nSuccessCount++;
+
+                print_road(arrRoad, nLen);
+                nEmptyGrid += 2;
+                bMove = 0;
+                break;
+            }
+        }
+
+        for (i = 0; i < nEmptyGrid && bMove; i++)
+        {
+            if ((arrRoad[nEmptyGrid - 1] < 0) && (nEmptyGrid < nLen - 2 && (arrRoad[nEmptyGrid - 2]) != arrRoad[nEmptyGrid + 1]))
+            {
+                swap(&arrRoad[nEmptyGrid - 1], &arrRoad[nEmptyGrid]);
+
+                if (nEmptyGrid == nLen - 1)
+                    nSuccessCount++;
+
+                print_road(arrRoad, nLen);
+                nEmptyGrid -= 1;
+                bMove = 0;
+                break;
+            }
+        }
+
+        for (i = nLen - 1; i > nEmptyGrid && bMove; i--)
+        {
+            if ((arrRoad[nEmptyGrid + 1] > 0) && (nEmptyGrid > 0 && arrRoad[nEmptyGrid + 2] != arrRoad[nEmptyGrid - 1]))
+            {
+                swap(&arrRoad[nEmptyGrid + 1], &arrRoad[nEmptyGrid]);
+
+                if (nEmptyGrid == 0)
+                    nSuccessCount++;
+
+                print_road(arrRoad, nLen);
+                nEmptyGrid += 1;
+                bMove = 0;
+                break;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int arrTest[7] = {-1, -1, -1, 0, 1, 1, 1};
+    int n = 0;
+
+    print_road(arrTest, 7);
+    move_frog(arrTest, 7, 3);
+
+    scanf("%d", &n);
+    return 0;
+}
--- suanfa.c
+++ suanfa.c
@@ -5,109 +5,139 @@
 #include <stdio.h>
 #include <string.h>
 
-const int W=101;
-const int D=100;
+const int W = 101;
+const int D = 100;
 
-int pos(int power){
-  return power+D;
+int pos(int power)
+{
+  return power + D;
 }
 
-int power(int pos){
-  return pos-D;
+int power(int pos)
+{
+  return pos - D;
 }
 
-void fill(int d[W],int t){
-  for (int i=0;i<W;i++) d[i]=t;
+void fill(int d[W], int t)
+{
+  for (int i = 0; i < W; i++)
+    d[i] = t;
 }
 
-
-void copy(int x[W],int y[W]){
-  for (int i=0;i<W;i++) y[i]=x[i];
+void copy(int x[W], int y[W])
+{
+  for (int i = 0; i < W; i++)
+    y[i] = x[i];
 }
 
-void assign(int x[W],char* n, char* d){
-  fill(x,0);
+void assign(int x[W], char *n, char *d)
+{
+  fill(x, 0);
   char *p;
-  p=n;
-  int L=strlen(n);
+  p = n;
+  int L = strlen(n);
   int i;
-  for (i=L-1;i>=0;i--,p++) x[pos(i)]=*p-'0';
-  L=strlen(d);
-  p=d;
-  for (i=-1;i>=-L;i--,p++) x[pos(i)]=*p-'0';
+  for (i = L - 1; i >= 0; i--, p++)
+    x[pos(i)] = *p - '0';
+  L = strlen(d);
+  p = d;
+  for (i = -1; i >= -L; i--, p++)
+    x[pos(i)] = *p - '0';
 }
 
-int equal(int x[W],int y[W]){
-  for (int i=W-1;i>0;i--)
-    if (x[i]!=y[i]) return 0;
+int equal(int x[W], int y[W])
+{
+  for (int i = W - 1; i > 0; i--)
+    if (x[i] != y[i])
+      return 0;
   return 1;
 }
 
-void mid(int x[W],int y[W],int z[W]){
-  z[0]=x[0]+y[0];
-  for (int i=1;i<W;i++){
-    z[i]=x[i]+y[i]+z[i-1]/10;
-    z[i-1]=z[i-1]%10;
+void mid(int x[W], int y[W], int z[W])
+{
+  z[0] = x[0] + y[0];
+  for (int i = 1; i < W; i++)
+  {
+    z[i] = x[i] + y[i] + z[i - 1] / 10;
+    z[i - 1] = z[i - 1] % 10;
   }
-  int i=0;
-  for (i=W-1;i>0;i--) {
-    z[i-1]+=(z[i]*10)%20;
-    z[i]/=2;
+  int i = 0;
+  for (i = W - 1; i > 0; i--)
+  {
+    z[i - 1] += (z[i] * 10) % 20;
+    z[i] /= 2;
   }
-  z[0]/=2;
+  z[0] /= 2;
 }
 
-void sqr(int x[W], int y[W]){
-  fill(y,0);
-  int i=0;
-  for (i=0;i<W;i++){
-    for (int j=0;j<W;j++){
-      int t=pos(power(i)+power(j));
-      if ((t>=0)&&(t<W)) y[t]+=x[i]*x[j];
+void sqr(int x[W], int y[W])
+{
+  fill(y, 0);
+  int i = 0;
+  for (i = 0; i < W; i++)
+  {
+    for (int j = 0; j < W; j++)
+    {
+      int t = pos(power(i) + power(j));
+      if ((t >= 0) && (t < W))
+        y[t] += x[i] * x[j];
     }
   }
-  for (i=0;i<W-1;i++){
-    y[i+1]+=y[i]/10;
-    y[i]=y[i]%10;
+  for (i = 0; i < W - 1; i++)
+  {
+    y[i + 1] += y[i] / 10;
+    y[i] = y[i] % 10;
   }
-  y[W-1]=y[W-1]%10;
+  y[W - 1] = y[W - 1] % 10;
 }
 
-int compare(int x[W],int y[W]){
-  for (int i=W-1;i>=0;i--){
-    if (x[i]<y[i]) return -1;
-    if (x[i]>y[i]) return 1;
+int compare(int x[W], int y[W])
+{
+  for (int i = W - 1; i >= 0; i--)
+  {
+    if (x[i] < y[i])
+      return -1;
+    if (x[i] > y[i])
+      return 1;
   }
   return 0;
 }
 
-void print(int x[W]){
-   int i;
-   for (i=W-1;i>=0;i--){
-     printf("%c",'0'+x[i]);
-     if (i==D) printf(".");
-   }
-   printf("\n");
+void print(int x[W])
+{
+  int i;
+  for (i = W - 1; i >= 0; i--)
+  {
+    printf("%c", '0' + x[i]);
+    if (i == D)
+      printf(".");
+  }
+  printf("\n");
 }
 
-void sqrt2(int x[W],int y[W]){
-   int a[W],b[W],c[W];
-   fill(a,0);
-   copy(x,b);
-   b[pos(0)]++;
-   while (!equal(a,b)) {
-      mid(a,b,c);
-      sqr(c,y);
-      if (compare(y,x)<0) copy(c,a);
-      else copy(c,b);
-   }
-   copy(c,y);
-   return;
+void sqrt2(int x[W], int y[W])
+{
+  int a[W], b[W], c[W];
+  fill(a, 0);
+  copy(x, b);
+  b[pos(0)]++;
+  while (!equal(a, b))
+  {
+    mid(a, b, c);
+    sqr(c, y);
+    if (compare(y, x) < 0)
+      copy(c, a);
+    else
+      copy(c, b);
+  }
+  copy(c, y);
+  return;
 }
 
-void main(){
-   int x[W],y[W];
-   assign(x,"2","0");
-   sqrt2(x,y);
-   print(y);
+void main()
+{
+  int x[W], y[W];
+  assign(x, "2", "0");
+  sqrt2(x, y);
+  print(y);
 }
